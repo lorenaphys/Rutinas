a5a8e54ff5f8ffe3d978434bcd96f9fea2e98d61
lorenaphys <lorena.030789@gmail.com>
2015-04-09 18:54:12 -0500

    aun no he conseguido que matlab no arroje las imagenes pero ya mejore las rutinas

--- Resultados/espDisA/movie.m
+++ Resultados/espDisA/movie.m
@@ -7,7 +7,6 @@ for ty=1:N
     
     load(['iter' num2str(ty)])
 
-    figure(1)
     clf
     hold on
     plot(u(25,:),'r')
@@ -15,26 +14,13 @@ for ty=1:N
     axis square
     grid on
     hold off
-    q=getframe;
-    qq(ty)=q;
+    qq(ty)=getframe;
     disp(ty)
 end
 
-
-figure(42)
-clf
-mm=0;
-for i=1:N
-   
-
-%%
-    Xn=frame2im(qq(i));
-    image(Xn)
-    axis equal
-    axis off% axis equal
- 
-     mk(i)=getframe(gcf);
-     %hold off
-    
+for i = 1:N
+    Xn = frame2im(qq(i));
+    mk(i) = getframe(gcf);
 end
+
 movie2avi(mk,'espDisA','compression','none')
