deef866338016ae70eb0828400ae800f5c17714d
lorenaphys <lorena.030789@gmail.com>
2015-04-14 17:42:56 -0500

    espero este commit sea exitoso

---
+++ Resultados/phiDynA/contornoA.m
@@ -0,0 +1,27 @@
+N = 401;
+Nx = 150;
+Ny = 50;
+nivel=0.0;
+    
+k = struct('cdata',[],'colormap',[]);
+
+for t = 1:N
+    
+    clear {XX YY ZZ ci c hi ci}
+    
+    load(['iter' num2str(t)])
+
+    clf 
+    contour(fi,[nivel nivel],'k','LineWidth',2);
+    hold on
+    contour(fiini,[nivel nivel],'r','LineWidth',2);
+    hold off
+    axis equal
+    axis([-5 30 0 30])
+    grid on
+    k(t) = getframe(gcf);
+    disp(t)
+   
+end 
+
+movie2avi(k,'contornoA','fps',50,'compression','none')
