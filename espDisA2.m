16dc32d1135b88f494ca2f0e85a7973f766d4892
lorenaphys <lorena.030789@gmail.com>
2015-04-15 18:41:20 -0500

    Rutinas que hacen la sugerencia de Mariana y cambios en las anteriores

--- espDisA2.m
+++ espDisA2.m
@@ -1,7 +1,6 @@
 clear all
 N=50;
 rad=10;
-radm=2.5*rad;
 dt=0.0005;
 C=0.1;
 bet=0.2;
@@ -14,29 +13,23 @@ gamma=0.0001;
 step=100;
 Finaliter=10;
 
-
 numero=0;
 
 fiin=1;
 fiout=-1;
 fi = zeros(N,N);
-u = zeros(N,N);
 
 for i=1:N
   for j=1:N
        r=sqrt((i-.5*(N+1))^2+(j-.5*(N+1))^2);
-       fi(i,j)=.5*(fiout-fiin)*tanh((r-rad)*0.8)+(fiout+fiin)*.8;
+       fi(i,j)=.5*(fiout-fiin)*(-tanh((r-rad)*0.8))+(fiout+fiin)*.8;
   end
 end
 
 fi(1:20,:)=1;
 fi(1:15,:)=-1;
 
-for i = 1:N
-    for j = 1:N
-        u(i,j) = exp(((j-25)^2 + (i-25)^2)/30);
-    end 
-end
+u=0.0*(rand(N,N)-.5);
 
 [stat,struc] = fileattrib;
 PathCurrent = struc.Name;
@@ -45,12 +38,12 @@ for q=1:220
 
 
 for iter=1:Finaliter
-    
-    
+   
+
     for iiter=1:step
- 
-        lapfi = delta2fun(fi);
-        
+       
+       lapfi = delta2fun(fi);
+
         mu=(fi-ep1*C*(1+bet*u)).*(fi.^2-1)-ep*lapfi;
                 
         lapmu = delta2fun(mu);
@@ -58,20 +51,36 @@ for iter=1:Finaliter
         lapu = delta2fun(u);
 
         F=2*(3*fi.^2-1-2*ep1*C*fi.*(1+bet*u)).*mu-ep*lapmu+gamma*lapu;
-        
+   
+        lapF = delta2fun(F);
+
         Fs=lapfi;
         
-        G=-2*mu*ep1*C*bet*(fi.^2-1)-D*lapu-gamma*lapfi;
-       
-        u=u-dt*G;
+        lapFs = delta2fun(Fs);
 
-        fi=fi-dt*(F-sigma*Fs);
+        G=-2*mu*ep1*C*bet*(fi.^2-1)-D*lapu-gamma*lapfi;
+        
+        lapG = delta2fun(G);
+
+        u=u+dt*lapG;
+
+        fi=fi+dt*(lapF-sigma*lapFs);
+        
+        lapF(1,:)=lapF(2,:);
+        lapF(N,:)=lapF(N-1,:);
+        lapF(:,1)=lapF(:,2);
+        lapF(:,N)=lapF(:,N-1);
+        
+        lapFs(1,:)=lapFs(2,:);
+        lapFs(N,:)=lapFs(N-1,:);
+        lapFs(:,1)=lapFs(:,2);
+        lapFs(:,N)=lapFs(:,N-1);
    
         fi(1,:)=fi(2,:);
         fi(N,:)=fi(N-1,:);
         fi(:,1)=fi(:,2);
         fi(:,N)=fi(:,N-1);
-         
+
         u(1,:)=u(2,:);
         u(N,:)=u(N-1,:);
         u(:,1)=u(:,2);
@@ -81,9 +90,9 @@ for iter=1:Finaliter
 
     numero=numero+1;
 
-    FolderName = 'espDisA';   
-    PathFolder = [PathCurrent '/Resultados/' FolderName];
-    [status,message,messageid] = mkdir([PathCurrent '/Resultados'], FolderName);
+    FolderName = 'espDisA2';   
+    PathFolder = [PathCurrent '/Resultados2/' FolderName];
+    [status,message,messageid] = mkdir([PathCurrent '/Resultados2'], FolderName);
     save([PathFolder ['/iter' num2str(numero)]]);
     
     disp(numero)
